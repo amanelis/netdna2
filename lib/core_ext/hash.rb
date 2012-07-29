b60f9d6a961aa7c2d69d008bd55291dbcb1ac554
Alex Manelis <amanelis@gmail.com>
2012-07-29 14:41:47 -0700

    Zone tests under way

--- lib/core_ext/hash.rb
+++ lib/core_ext/hash.rb
@@ -3,17 +3,9 @@ require "addressable/uri"
 class Hash
   def to_q
     return '' if self.empty?
-    
-    # Use the URI lib to create parameter
-    # like hash of the values needed to be 
-    # sent to the API.
-    uri = Addressable::URI.new
-    
-    # Here is the conversion
-    queries = uri.query_values self
-  
-    # Old way that was not consistent
-    # return self.send(:to_query)
-    return queries
+    queries = ''
+    map { |k,v| queries += "#{k}=#{v}&" }
+    queries.chomp!('&')
+    queries
   end
 end
--- spec/netdna2/zones_spec.rb
+++ spec/netdna2/zones_spec.rb
@@ -20,25 +20,63 @@ describe NetDna2::Zones do
     end
     
     it '@zones.company_alias' do
-      @zones.company_alias.should_not == nil && @zones.company_alias.should == NETDNA_COMPANY_ALIAS
+      @zones.company_alias.should == NETDNA_COMPANY_ALIAS && @zones.company_alias.should_not == nil
     end
     
     it '@zones.consumer_key' do
-      @zones.consumer_key.should_not == nil && @zones.consumer_key.should == NETDNA_CONSUMER_KEY
+      @zones.consumer_key.should == NETDNA_CONSUMER_KEY && @zones.consumer_key.should_not == nil
     end
     
     it '@zones.consumer_secret' do
-      @zones.consumer_secret.should_not == nil && @zones.consumer_secret.should == NETDNA_CONSUMER_SECRET
+      @zones.consumer_secret.should == NETDNA_CONSUMER_SECRET && @zones.consumer_secret.should_not == nil
     end
     
-    # Zone API endpoint methods
+    ## list_zones
+    ##########################################
     it '@zones.list_zones w/out parameters' do
       @zones.list_zones.should_not == nil 
     end
     
-    it '@zones.list_zones WITH parameters' do
-      @zones.list_zones(page: 2).should_not == nil 
+    it '@zones.list_zones WITH parameters(page: 2) response should return' do
+      response = @zones.list_zones(page: 2)
+      response.should_not == nil && response.class.should == Hash
     end
+    
+    it '@zones.list_zones WITH parameters(page: 2) should return :page => 2' do
+      response = @zones.list_zones(page: 2)
+      response_page = response['data']['page'].to_i
+      response_page.should == 2 && response['data'].class.should == Hash
+    end
+    
+    it '@zones.list_zones WITH parameters(page: 2) should return :zones => []' do
+      response = @zones.list_zones(page: 2)
+      response_zones = response['data']
+      response_zones.should_not == nil && response_zones.class.should == Array
+    end
+  
+    
+    ## zone_summary 
+    ##########################################
+    
     
   end
 end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
