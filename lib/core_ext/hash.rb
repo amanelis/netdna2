a07bb6179d9c4e6623403a37a741dbe31ffade9b
Alex Manelis <amanelis@gmail.com>
2012-07-29 14:53:40 -0700

    Hash method fully tested

--- lib/core_ext/hash.rb
+++ lib/core_ext/hash.rb
@@ -2,7 +2,7 @@ require "addressable/uri"
 
 class Hash
   def to_q
-    return '' if self.empty?
+    return '' if self.empty? || self.nil?
     queries = ''
     map { |k,v| queries += "#{k}=#{v}&" }
     queries.chomp!('&')
---
+++ spec/core_ext/hash_spec.rb
@@ -0,0 +1,30 @@
+require 'spec_helper'
+require File.join(File.dirname(__FILE__), '../../', 'lib/core_ext/hash')
+
+
+describe Hash do
+  context "to_q" do
+    before(:all) do
+      @hash_0 = {}
+      @hash_1 = {"page"=>1, "pages"=>10, "page_size"=>"50", "current_page_size"=>50, "total"=>494}
+      @hash_2 = {page: 1, pages: 10, page_size: 50, current_page_size: 50, total: 494}
+      @result = "page=1&pages=10&page_size=50&current_page_size=50&total=494"
+    end
+    
+    it "should return '' if an empty or nil {} is called" do
+      method = @hash_0.to_q
+      method.should == '' && method.class.should == String
+    end
+    
+    it "should return a correctly formatted response for a 1.8.7 formatted Hash:{}" do
+      method = @hash_1.to_q
+      method.should == @result && method.class.should == String
+    end
+    
+    it "should return a correctly formatted response for a 1.9.2 formatted Hash:{}" do
+      method = @hash_2.to_q
+      method.should == @result && method.class.should == String
+    end
+    
+  end
+end
